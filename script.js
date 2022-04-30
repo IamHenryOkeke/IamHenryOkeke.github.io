54404077a02a719f4cea7145917d3dcac8060efc
Okeke Nmesomachukwu Henry <89491787+IamHenryOkeke@users.noreply.github.com>
2022-04-30 09:53:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let quote = "If you believe, nothing is impossible";
+
+let x1 = "42" + 7;
+
+let y2 = "42" - 7;
+
+let coding = "cool";
+
+let coding2 = "trendy and fun";
+
+console.log(coding)
+
+console.log(coding2)
+
+let firstMeal = "Salad";
+
+let secondMeal = "Pasta";
+
+let thirdMeal = "Jollof Rice";
+
+let fourth = "Ice Cream";
+
+//1. 
+let x = 31 + 7;
+// enter your guess where the '0' is on the next line
+let guessOne = 38;//WRITE ANSWER = 38
+console.log(x);
+
+//2.
+let y = 8;
+let food = "delicious";
+// Enter your guess on the next two lines, where the 'y' and 'food' is
+console.log(y); //WRITE ANSWER = 8
+console.log(food); //WRITE ANSWER = delicious
+
+//3.
+let y3 = (9 * 6) / 3 + 2;
+// Enter your guess on the next line, where the '0' is
+let guessTwo = 20;//WRITE ANSWER = 20 
+console.log(y3);
+
+//4.
+let z = "We learn to code for " + "a better future, better life, and better society.";
+// Enter your guess inside the "" below
+// Be careful! The text has to match exactly.
+let guessThree = "We learn to code for a better future, better life, and better society.";//WRITE ANSWER = We learn to code for a better future, better life, and better society.
+console.log(z);
